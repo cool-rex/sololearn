d1b3f354b4aa05b3fbf221d7bbd0a9d94266be84
cool-rex <rbrefo2@gmail.com>
2023-09-21 11:20:09 +0000

    update

---
+++ Template Literals.js
@@ -0,0 +1,19 @@
+// You need to make country cards for a school project.
+// The given program takes the country and its capital name as input.
+
+// Task
+// Complete the code to return a string in the format you are given in the sample output:
+
+// Sample Input
+// Portugal
+// Lisbon
+
+// Sample Output
+// Country: Portugal, Capital: Lisbon
+
+
+let country = readLine();
+let capital = readLine();
+
+//complete the code
+console.log(`Country: ${country}, Capital: ${capital} `);
