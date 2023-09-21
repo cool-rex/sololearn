e84f6cd20d7893d9e9500f4b34036e2b695f7e37
cool-rex <rbrefo2@gmail.com>
2023-09-21 11:10:52 +0000

    update

---
+++ Strings.js
@@ -0,0 +1,10 @@
+
+// You are given code that should output a string with quotes.
+
+// However, it contains errors.
+
+// Task
+// Fix the given code to generate the expected output.
+
+//fix the code
+console.log('I\'m learning JavaScript. It\'s easy');
