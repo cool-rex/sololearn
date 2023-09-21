ea81cdea055628a8dce6af56d6d2a879893eb5b7
cool-rex <rbrefo2@gmail.com>
2023-09-21 11:25:51 +0000

    update

---
+++ Getting User Input.js
@@ -0,0 +1,21 @@
+// You are organizing an important event and you should greet each participant with a welcome message․
+
+// The given program takes the name of the guest as input.
+
+// Task
+// Complete the program to output the welcome message in the format given in the sample output.
+
+// Sample Input
+// Tom
+
+// Sample Output
+// Welcome, Tom!
+
+
+
+let name = readLine();
+
+//your code goes here
+
+
+console.log("Welcome, " + name +"!");
