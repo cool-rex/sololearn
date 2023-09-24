e6f8a5d2ab551de01a3d44b118e859ed923ec15a
cool-rex <rbrefo2@gmail.com>
2023-09-24 21:53:04 +0000

    update

---
+++ Functions.js
@@ -0,0 +1,18 @@
+
+// // We have a function that outputs "Welcome, user" as it is called. We want to make it more personalized, so redesign the given function so that it will take the given input as the name of the user and output the welcome message with it.
+
+// Sample Input
+// Tommy
+
+// Sample Output
+// Welcome, Tommy
+
+
+function welcome(){
+    let name = readLine();
+    //redesign function
+    console.log('Welcome, ' + name);
+}
+
+//call the function
+welcome('Rexford');
