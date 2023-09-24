53dff209076345b4224dddc5e2fad9c0befef0a0
cool-rex <rbrefo2@gmail.com>
2023-09-24 23:31:36 +0000

    update

---
+++ Feet to Inches Converter.js
@@ -0,0 +1,22 @@
+
+// You need to make a function that converts a foot value to inches.
+// 1 foot has 12 inches.
+// The given code takes the foot value as input and passes it to the convert function.
+
+// Task
+// Complete the code by defining a convert() function that takes the foot value as an argument and outputs the inches value.
+
+// Input Example
+// 8
+
+// Output Example
+// 96
+
+
+let foot = parseInt(readLine(), 10);
+
+/* Define the convert() function */
+function convert(){
+   console.log( 12 * foot);
+}
+convert();
