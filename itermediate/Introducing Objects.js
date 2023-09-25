f36c2fb11ff354c49859dc853362f3ccb4e364bc
cool-rex <rbrefo2@gmail.com>
2023-09-25 00:26:14 +0000

    update

---
+++ itermediate/Introducing Objects.js
@@ -0,0 +1,16 @@
+
+// The given class represents a cuboid (e.g. a rectangular prism) that contains the properties of length, width, and height.
+
+// Complete the program to calculate and output the volume of the given cuboid to the console
+
+
+var cuboid = {
+    length: 25,
+    width: 50,
+    height: 200
+};
+
+//your code goes here
+let res = cuboid.length*cuboid.width*cuboid.height;
+
+console.log(res);
