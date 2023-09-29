9e53fd99107437a914e691fe8ed286b5da3bbfcb
cool-rex <rbrefo2@gmail.com>
2023-09-29 23:20:14 +0000

    update

---
+++ itermediate/Store Manager.js
@@ -0,0 +1,5 @@
+
+// You are working on a Store Manager program, which stores the prices in an array.
+// You need to add functionality to increase the prices by the given amount.
+// The increase variable is taken from user input. You need to increase all the prices in the given array by that amount and output to the console the resulting array.
+
